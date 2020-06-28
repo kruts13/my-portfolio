5e88a3cf96674ed7fa1a6391d6dae2a5b69a8020
Kruti Dave <kruti.dave@qualicom.com>
2020-06-27 21:27:18 -0400

    create an accordion.

--- app.js
+++ app.js
@@ -1,55 +1,19 @@
-const getCurrentImg = document.getElementById('current');
-const getAllImages = document.querySelectorAll('.imgs img');
-const getAllBtns = document.querySelectorAll('.nav button');
-const getBtnLeft = document.getElementById('btn-left');
-const getBtnRight = document.getElementById('btn-right');
-let currentBtnIndex = 0;
+const getHeaders = document.querySelectorAll('.header');
 
-function removeActiveClass () {
-    const getAllActives = document.querySelectorAll('.active');
-    getAllActives.forEach(e => {
-        e.className = e.className.replace('active', '');
-    })
-}
-
-function addActiveClass(btnElem, imgElem) {
-    btnElem.classList.add('active');
-    imgElem.classList.add('active');
-}
-
-getAllImages.forEach((elem, index) => {
+getHeaders.forEach((elem, index) => {
     elem.addEventListener('click', e => {
-        getCurrentImg.src = elem.src;
-        removeActiveClass();
-        addActiveClass(getAllBtns[index], elem);
-        currentBtnIndex = index;
+        let accor = e.target.nextElementSibling;
+        let expandBtn = document.getElementById('expand' + index);
+        let collBtn = document.getElementById('coll' + index);
+
+        if (parseInt(accor.style.height) > 0) {
+            accor.style.height = 0;
+            expandBtn.style.display = 'inline-block';
+            collBtn.style.display = 'none';
+        } else {
+            accor.style.height = 50 + 'px';
+            expandBtn.style.display = 'none';
+            collBtn.style.display = 'inline-block';
+        }
     })
-});
-
-getAllBtns.forEach((elem, index) => {
-    elem.addEventListener('click', e => {
-        getCurrentImg.src = getAllImages[index].src;
-        removeActiveClass();
-        addActiveClass(elem, getAllImages[index]);
-        elem.classList.add('active');
-        currentBtnIndex = index;
-    });
-});
-
-getBtnLeft.addEventListener('click', e => {
-    if (currentBtnIndex > 0) {
-        removeActiveClass();
-        currentBtnIndex -= 1;
-        getCurrentImg.src = getAllImages[currentBtnIndex].src;  
-        addActiveClass(getAllBtns[currentBtnIndex], getAllImages[currentBtnIndex]);
-    }
-});
-
-getBtnRight.addEventListener('click', e => {
-    if (currentBtnIndex < getAllBtns.length - 1) {
-        removeActiveClass();
-        currentBtnIndex += 1;
-        getCurrentImg.src = getAllImages[currentBtnIndex].src;  
-        addActiveClass(getAllBtns[currentBtnIndex], getAllImages[currentBtnIndex]);
-    }
-});
+})
