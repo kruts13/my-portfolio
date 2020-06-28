b80f32c2cc24abd4d499b9b8c28fef5694f88b79
Kruti Dave <kruti.dave@qualicom.com>
2020-06-27 21:41:50 -0400

    create five star widget.

--- app.js
+++ app.js
@@ -1,13 +1,33 @@
-const getListElemArr = Array.from(document.querySelector('.nav').children);
-getListElemArr.forEach((newElemTab, index) => {
-    newElemTab.addEventListener('click', e => {
-        let newElemTabPane = document.getElementById('tab' + index);
-        let activeTabs = Array.from(document.querySelectorAll('.active'));
-        activeTabs.forEach(elem => {
-            elem.className = elem.className.replace('active', '');
-        });
+const getCircle = document.querySelectorAll('.star-button');
 
-        newElemTab.classList.add('active');
-        newElemTabPane.classList.add('active');
-    })
+getCircle.forEach(circle => {
+    circle.addEventListener('click', e => {
+        checkHover(e, 'change-color-gold');
+        e.target.classList.add('change-color-gold');
+    });
 });
+
+function callHover(e) {
+    console.log(e);
+    checkHover(e, 'hover-gold');
+}
+
+function checkHover(e, goldClass) {
+    let prevElem = e.target;
+    let nextElem = e.target.nextElementSibling;
+    while (prevElem.classList.contains('star-button')) {
+        prevElem.classList.add(goldClass);
+        prevElem = prevElem.previousElementSibling;
+    }
+
+    while (nextElem.classList.contains(goldClass)) {
+        nextElem.classList.remove(goldClass);
+        nextElem = nextElem.nextElementSibling;
+    }
+}
+
+function greyAll(elem) {
+    getCircle.forEach(circle => {
+        circle.classList.remove('hover-gold');
+    });
+}
